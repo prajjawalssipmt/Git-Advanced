8a3fb867764d77df73aec8a957249de8d385c209
prajjawalssipmt <tanmayprajjawalgupta@gamil.com>
2026-05-29 22:30:22 +0530

    modify my.java

--- my.java
+++ my.java
@@ -2,7 +2,6 @@ public class my {
     public static void main(String[] args) {
         System.out.println("Welcome to my world");
         System.out.println("This is a simple Java program.");
-        System.out.println("Enjoy coding!");
         System.out.println("Have a great day!");
     }
 }
